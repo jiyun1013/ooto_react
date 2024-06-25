e3809c0002c7115ec5cb2112bada7e57949dc303
jiyun1013 <rkf0645@naver.com>
2024-06-25 15:41:22 +0900

    로딩창

--- src/App.js
+++ src/App.js
@@ -3,22 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          리액트
-          수정했을 때 어떻게 되는지 궁금해!
-        </a>
-      </header>
+    <div class="box">
+        <div class="circle1"></div>
+        <div class="circle2"></div>
     </div>
   );
 }
